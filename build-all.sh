a0c9f30ae7746d2fe4394b91cbede70223ad8e4d
Taylor Holliday <wtholliday@gmail.com>
2023-02-06 14:59:39 -0800

    Log to standard output.

--- build-all.sh
+++ build-all.sh
@@ -246,29 +246,21 @@ set -e
 # Change to the root directory
 cd "${rootdir}"
 
-# Set up a logfile
-mkdir -p "${logdir}"
-logfile="${logdir}/build-$(date -u +%F-%H%M).log"
-rm -f "${logfile}"
-
 # Checkout the correct branch for each tool
-echo "Checking out GIT trees" >> "${logfile}"
-echo "======================" >> "${logfile}"
+echo "Checking out GIT trees"
+echo "======================"
 
 echo "Checking out GIT trees ..."
 if ! ${rootdir}/toolchain/avr32-versions.sh ${rootdir} ${autocheckout} \
-         ${autopull} >> "${logfile}" 2>&1
+         ${autopull}
 then
     echo "ERROR: Failed to checkout GIT versions of tools"
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Build the tool chain
-echo "START AVR32 TOOLCHAIN BUILD: $(date)" >> "${logfile}"
 echo "START AVR32 TOOLCHAIN BUILD: $(date)"
 
-echo "Installing in ${installdir}" >> "${logfile}" 2>&1
 echo "Installing in ${installdir}"
 
 # We'll need the tool chain on the path.
@@ -276,8 +268,8 @@ PATH=${installdir}/bin:$PATH
 export PATH
 
 # Configure binutils
-echo "Configuring binutils" >> "${logfile}"
-echo "====================" >> "${logfile}"
+echo "Configuring binutils"
+echo "===================="
 
 echo "Configuring binutils ..."
 
@@ -291,29 +283,27 @@ if "${rootdir}/binutils"/configure --target=avr32 \
         --disable-nls --disable-werror \
         --with-pkgversion="AVR32 toolchain ${VERSION} (built $(date +%Y%m%d))" \
         --with-bugurl="http://www.atmel.com/avr" \
-        --prefix=${installdir} >> "${logfile}" 2>&1
+        --prefix=${installdir} 
 then
     echo "  finished configuring binutils"
 else
     echo "ERROR: binutils configure failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Build binutils
-echo "Building binutils" >> "${logfile}"
-echo "=================" >> "${logfile}"
+echo "Building binutils"
+echo "================="
 
 echo "Building binutils ..."
 
 # Per Arnold magic to get headers to reconfigure. We really need to get this
 # sorted properly, so plain make works OK.
-if make ${parallel} all-bfd TARGET-bfd=headers >> "${logfile}" 2>&1
+if make ${parallel} all-bfd TARGET-bfd=headers
 then
     echo "  finished building BFD headers"
 else
     echo "ERROR: BFD headers build failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
@@ -322,35 +312,33 @@ rm -f bfd/Makefile
 
 # Build
 cd "${bd_binutils}"
-if make ${parallel} all-build all-binutils all-gas all-ld >> "${logfile}" 2>&1
+if make ${parallel} all-build all-binutils all-gas all-ld
 then
     echo "  finished building binutils"
 else
     echo "ERROR: binutils build failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Install binutils
-echo "Installing binutils" >> "${logfile}"
-echo "===================" >> "${logfile}"
+echo "Installing binutils"
+echo "==================="
 
 echo "Installing binutils ..."
 
 # Install
 cd "${bd_binutils}"
-if make install-binutils install-gas install-ld >> "${logfile}" 2>&1
+if make install-binutils install-gas install-ld 
 then
     echo "  finished installing binutils"
 else
     echo "ERROR: binutils install failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Configure gcc bootstrap (pre-Newlib)
-echo "Configuring gcc (bootstrap)" >> "${logfile}"
-echo "===========================" >> "${logfile}"
+echo "Configuring gcc (bootstrap)" 
+echo "===========================" 
 
 echo "Configuring gcc (bootstrap) ..."
 
@@ -367,53 +355,50 @@ if "${rootdir}/gcc"/configure --target=avr32 \
         --enable-languages=c \
         --with-pkgversion="AVR32 toolchain ${VERSION} (built $(date +%Y%m%d))" \
         --with-bugurl="http://www.atmel.com/avr" \
-        --prefix=${installdir} >> "${logfile}" 2>&1
+        --prefix=${installdir} 
 then
     echo "  finished configuring gcc (bootstrap)"
 else
     echo "ERROR: gcc (bootstrap) configure failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Build gcc bootstrap version
-echo "Building gcc (bootstrap)" >> "${logfile}"
-echo "========================" >> "${logfile}"
+echo "Building gcc (bootstrap)" 
+echo "========================" 
 
 echo "Building gcc (bootstrap) ..."
 
 # Build.
 cd "${bd_gcc_bs}"
 if make ${libppl} ${parallel_gcc} all-build all-gcc \
-        all-target-libgcc >> "${logfile}" 2>&1
+        all-target-libgcc 
 then
     echo "  finished building gcc (bootstrap)"
 else
     echo "ERROR: gcc (bootstrap) build failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Install gcc
-echo "Installing gcc (bootstrap)" >> "${logfile}"
-echo "==========================" >> "${logfile}"
+echo "Installing gcc (bootstrap)" 
+echo "==========================" 
 
 echo "Installing gcc (bootstrap) ..."
 
 # Install
 cd "${bd_gcc_bs}"
-if make install-gcc install-target-libgcc >> "${logfile}" 2>&1
+if make install-gcc install-target-libgcc 
 then
     echo "  finished installing gcc (bootstrap)"
 else
     echo "ERROR: gcc (bootstrap) install failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Configure newlib
-echo "Configuring newlib" >> "${logfile}"
-echo "==================" >> "${logfile}"
+echo "Configuring newlib" 
+echo "==================" 
 
 echo "Configuring newlib ..."
 
@@ -431,52 +416,49 @@ if "${rootdir}/newlib"/configure --target=avr32 \
         --enable-target-optspace \
         --with-pkgversion="AVR32 toolchain ${VERSION} (built $(date +%Y%m%d))" \
         --with-bugurl="http://www.atmel.com/avr" \
-        --prefix=${installdir} >> "${logfile}" 2>&1
+        --prefix=${installdir} 
 then
     echo "  finished configuring newlib"
 else
     echo "ERROR: newlib configure failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Build newlib
-echo "Building newlib" >> "${logfile}"
-echo "========================" >> "${logfile}"
+echo "Building newlib" 
+echo "========================" 
 
 echo "Building newlib ..."
 
 # Build
 cd "${bd_newlib}"
-if make ${parallel} all-target-libgloss all-target-newlib >> "${logfile}" 2>&1
+if make ${parallel} all-target-libgloss all-target-newlib 
 then
     echo "  finished building newlib"
 else
     echo "ERROR: newlib build failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Install newlib
-echo "Installing newlib" >> "${logfile}"
-echo "==========================" >> "${logfile}"
+echo "Installing newlib" 
+echo "==========================" 
 
 echo "Installing newlib ..."
 
 # Install
 cd "${bd_newlib}"
-if make install-target-libgloss install-target-newlib >> "${logfile}" 2>&1
+if make install-target-libgloss install-target-newlib 
 then
     echo "  finished installing newlib"
 else
     echo "ERROR: newlib install failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Configure gcc full (post-Newlib)
-echo "Configuring gcc (full)" >> "${logfile}"
-echo "======================" >> "${logfile}"
+echo "Configuring gcc (full)" 
+echo "======================" 
 
 echo "Configuring gcc (full) ..."
 
@@ -497,54 +479,51 @@ if "${rootdir}/gcc"/configure --target=avr32 \
         --disable-shared \
         --with-pkgversion="AVR32 toolchain ${VERSION} (built $(date +%Y%m%d))" \
         --with-bugurl="http://www.atmel.com/avr" \
-        --prefix=${installdir} >> "${logfile}" 2>&1
+        --prefix=${installdir} 
 then
     echo "  finished configuring gcc (full)"
 else
     echo "ERROR: gcc (full) configure failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Build gcc full version
-echo "Building gcc (full)" >> "${logfile}"
-echo "===================" >> "${logfile}"
+echo "Building gcc (full)" 
+echo "===================" 
 
 echo "Building gcc (full) ..."
 
 # Build.
 cd "${bd_gcc}"
 if make ${libppl} ${parallel_gcc} all-build all-gcc all-target-libgcc \
-        all-target-libstdc++-v3 >> "${logfile}" 2>&1
+        all-target-libstdc++-v3 
 then
     echo "  finished building gcc (full)"
 else
     echo "ERROR: gcc (full) build failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Install gcc
-echo "Installing gcc (full)" >> "${logfile}"
-echo "=====================" >> "${logfile}"
+echo "Installing gcc (full)" 
+echo "=====================" 
 
 echo "Installing gcc (full) ..."
 
 # Install
 cd "${bd_gcc}"
 if make install-gcc install-target-libgcc install-target-libstdc++-v3 \
-        >> "${logfile}" 2>&1
+        
 then
     echo "  finished installing gcc (full)"
 else
     echo "ERROR: gcc (full) install failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Configure gdb
-echo "Configuring gdb" >> "${logfile}"
-echo "===============" >> "${logfile}"
+echo "Configuring gdb" 
+echo "===============" 
 
 echo "Configuring gdb ..."
 
@@ -558,53 +537,49 @@ if "${rootdir}/gdb"/configure --target=avr32 \
         --disable-nls --disable-werror --with-python \
         --with-pkgversion="AVR32 toolchain ${VERSION} (built $(date +%Y%m%d))" \
         --with-bugurl="http://www.atmel.com/avr" \
-        --prefix=${installdir} >> "${logfile}" 2>&1
+        --prefix=${installdir} 
 then
     echo "  finished configuring gdb"
 else
     echo "ERROR: gdb configure failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Build gdb
-echo "Building gdb" >> "${logfile}"
-echo "============" >> "${logfile}"
+echo "Building gdb" 
+echo "============" 
 
 echo "Building gdb ..."
 
 # Build
 cd "${bd_gdb}"
-if make ${parallel} all-build all-gdb all-sim >> "${logfile}" 2>&1
+if make ${parallel} all-build all-gdb all-sim 
 then
     echo "  finished building gdb"
 else
     echo "ERROR: gdb build failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # Install gdb
-echo "Installing gdb" >> "${logfile}"
-echo "==============" >> "${logfile}"
+echo "Installing gdb" 
+echo "==============" 
 
 echo "Installing gdb ..."
 
 # Install
 cd "${bd_gdb}"
-if make install-gdb install-sim >> "${logfile}" 2>&1
+if make install-gdb install-sim 
 then
     echo "  finished installing gdb"
 else
     echo "ERROR: gdb install failed."
-    echo "- see ${logfile}"
     exit 1
 fi
 
 # All tools built
-echo "DONE AVR32: $(date)" >> "${logfile}"
+echo "DONE AVR32: $(date)" 
 echo "DONE AVR32: $(date)"
-echo  "- see ${logfile}"
 
 # Link to the defined place. Note the introductory comments about the need to
 # specify explicitly the install directory.
